e41bf3859a3c18091683a24c8dab8f08f3c19681
Caique Fernandes <caique.fernandes94@hotmail.com>
2022-02-23 18:07:47 -0300

    make it better

--- scr/app.js
+++ scr/app.js
@@ -23,8 +23,18 @@ const loginRoute = require('./routes/login-route');
 const indexRoute = require('./routes/index-route');
 const sitesRoute = require('./routes/site-route');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Habilita o CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', indexRoute);
 app.use('/login', loginRoute);
