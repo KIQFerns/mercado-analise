6b4f78fea916cd7e69a088b529d27c80f1b41398
Caique Fernandes <caique.fernandes94@hotmail.com>
2022-02-24 18:59:07 -0300

    make it better

--- scr/controllers/categorie-controller.js
+++ scr/controllers/categorie-controller.js
@@ -63,6 +63,6 @@ exports.getattribute = async (req, res, next) => {
             console.log(error);
         });
 
-    //res.render('pages/categorie', { data: global.data });
+    res.render('pages/categorie-attribute', { data: global.data });
     return;
 };
