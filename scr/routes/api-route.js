3470d6670edd3df92aa85235424c3b46b6ef999b
Caique Fernandes <caique.fernandes94@hotmail.com>
2022-02-24 17:48:01 -0300

    make it better

--- scr/routes/api-route.js
+++ scr/routes/api-route.js
@@ -7,7 +7,4 @@ const controller = require('../controllers/api-controller');
 //envia dados sobre os sites para a página sites
 router.get('/', controller.get);
 
-//envia dados sobre asd categorias para a página sites
-router.get('/categories', controller.getcategorie);
-
 module.exports = router;
