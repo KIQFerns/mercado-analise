74eb197fc666c28658fabd606f6f67812ab9eecb
Caique Fernandes <caique.fernandes94@hotmail.com>
2022-02-24 18:00:50 -0300

    make it better

--- scr/controllers/categorie-controller.js
+++ scr/controllers/categorie-controller.js
@@ -8,27 +8,27 @@ exports.getcategorie = async (req, res, next) => {
     var bearer = 'Bearer ';
     var token = global.access_token;
     console.log(bearer + token);
-  
+
     var axios = require('axios');
-  
+
     var config = {
-      method: 'get',
-      url: 'https://api.mercadolibre.com/sites/MLB/categories',
-      headers: {
-        'Authorization': bearer + token
-      }
+        method: 'get',
+        url: 'https://api.mercadolibre.com/sites/MLB/categories',
+        headers: {
+            'Authorization': bearer + token
+        }
     };
-  
+
     axios(config)
-      .then(function (res) {
-        console.log(JSON.stringify(res.data));
-        global.categoriedata = [];
-        global.categoriedata = res.data;
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    
-      res.render('pages/categorie', {categoriedata: global.categoriedata } );
-      return; 
-  };
+        .then(function (res) {
+            console.log(JSON.stringify(res.data));
+            global.data = [];
+            global.edata = res.data;
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    res.render('pages/categorie', { categoriedata: global.data });
+    return;
+};
