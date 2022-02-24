1f999aecea1713e7d2ad19f5e5f721a32ef05105
Caique Fernandes <caique.fernandes94@hotmail.com>
2022-02-24 11:51:38 -0300

    make it better

--- scr/controllers/site-controller.js
+++ scr/controllers/site-controller.js
@@ -4,7 +4,6 @@
 const https = require('https');
 var axios = require('axios');
 
-
 // conteudo /sites
 exports.get = async (req, res, next) => {
   var bearer = 'Bearer ';
@@ -31,34 +30,5 @@ exports.get = async (req, res, next) => {
       console.log(error);
     });
   
-    return next();   
-};
-
-// conteudo /sites/MLB/categories
-exports.get = async (req, res, next) => {
-  var bearer = 'Bearer ';
-  var token = global.access_token;
-  console.log(bearer + token);
-
-  var axios = require('axios');
-
-  var config = {
-    method: 'get',
-    url: 'https://api.mercadolibre.com/MLB/categories',
-    headers: {
-      'Authorization': bearer + token
-    }
-  };
-
-  axios(config)
-    .then(function (res) {
-      console.log(JSON.stringify(res.data));
-      global.sitecategoriedata = [];
-      global.sitecategoriedata = res.data;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  
-    res.render('pages/site', {data: global.sitecategoriedata } );   
+    res.render('pages/site', {data: global.sitedata } );   
 };
