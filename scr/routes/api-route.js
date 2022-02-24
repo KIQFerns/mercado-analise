3ba46944ed3b2bd0bd8a1f8858f6e25d2b6d8871
Caique Fernandes <caique.fernandes94@hotmail.com>
2022-02-24 16:22:28 -0300

    make it better

--- scr/routes/api-route.js
+++ scr/routes/api-route.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/api-controller');
 
-//envia dados sobre usuário para a página sites
+//envia dados sobre usuário para a página sitess
 router.get('/sites', controller.get);
 
 module.exports = router;
