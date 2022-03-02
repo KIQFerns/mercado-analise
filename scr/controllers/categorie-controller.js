b3069bc036dec7bb5530324e4502c98efcd17002
Caique Fernandes <caique.fernandes94@hotmail.com>
2022-03-02 11:33:24 -0300

    make it better

--- scr/controllers/categorie-controller.js
+++ scr/controllers/categorie-controller.js
@@ -39,7 +39,7 @@ exports.getattribute = async (req, res, next) => {
     var token = global.access_token;
     console.log(bearer + token);
 
-    var url = 'https://api.mercadolibre.com/sites/MLB/search?category=';
+    var url = 'https://api.mercadolibre.com/categories/';
     var id = req.params.id;
     console.log(url + id);
 
