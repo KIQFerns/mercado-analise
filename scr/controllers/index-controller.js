1a812e1e4370599f0db27de664c11b041774598b
Caique Fernandes <caique.fernandes94@hotmail.com>
2022-03-10 14:00:01 -0300

    arrumando as rotas por função

--- scr/controllers/index-controller.js
+++ scr/controllers/index-controller.js
@@ -76,7 +76,7 @@ exports.getuser = (req, res, next) => {
       global.first_name = res.data.first_name;
       global.last_name = res.data.last_name;
 
-      res.render('pages/index', {
+      next.render('pages/index', {
         user_id: global.user_id,
         first_name: global.first_name,
         last_name: global.last_name
