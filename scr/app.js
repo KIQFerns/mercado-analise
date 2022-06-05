33998926ab1be60cd5b02823dac213436560299c
Caique Fernandes <caique.fernandes94@hotmail.com>
2022-06-05 16:51:05 -0300

    alteração nomes

--- scr/app.js
+++ scr/app.js
@@ -1,55 +1,55 @@
-'use strict'
+    'use strict'
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
-const mongoose = require('mongoose');
-//const config = require('./config');
+    const express = require('express');
+    const bodyParser = require('body-parser');
+    const dotenv = require('dotenv').config();
+    const mongoose = require('mongoose');
+    //const config = require('./config');
 
-const app = express();
-const router = express.Router();
+    const app = express();
+    const router = express.Router();
 
-//Connecta ao banco
-//mongoose.connect(config.connectionString);
+    //Connecta ao banco
+    //mongoose.connect(config.connectionString);
 
-// Carrega os Models cc
+    // Carrega os Models cc
 
-// configura as views como ejs
-app.set('view engine', 'ejs');
-app.set('views', './scr/views');
+    // configura as views como ejs
+    app.set('view engine', 'ejs');
+    app.set('views', './scr/views');
 
-//carrega as rotas
-const indexRoute = require('./routes/index-route');
-const apiRoute = require('./routes/api-route');
-const categorieRoute = require('./routes/categorie-route');
-const itemRoute = require('./routes/item-route');
-const productRoute = require('./routes/product-route');
-const userRoute = require('./routes/user-route');
-const visitRoute = require('./routes/visit-route');
-const trendsRoute = require('./routes/trends-route');
-const highlightsRoute = require('./routes/highlights-route');
+    //carrega as rotas
+    const indexRoute = require('./routes/index-route');
+    const apiRoute = require('./routes/api-route');
+    const categorieRoute = require('./routes/categorie-route');
+    const itemRoute = require('./routes/item-route');
+    const productRoute = require('./routes/product-route');
+    const userRoute = require('./routes/user-route');
+    const visitRoute = require('./routes/visit-route');
+    const trendsRoute = require('./routes/trends-route');
+    const highlightsRoute = require('./routes/highlights-route');
 
-app.use(bodyParser.json({
-    limit: '1gb'
-}));
-app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json({
+        limit: '1gb'
+    }));
+    app.use(bodyParser.urlencoded({ extended: false }));
 
-// Habilita o CORS
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-});
+    // Habilita o CORS
+    app.use(function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        next();
+    });
 
-app.use('/', indexRoute);
-app.use('/api', apiRoute);
-app.use('/categories', categorieRoute);
-app.use('/items', itemRoute);
-app.use('/products', productRoute);
-app.use('/users', userRoute);
-app.use('/visits', visitRoute);
-app.use('/trends', trendsRoute);
-app.use('/highlights', highlightsRoute);
+    app.use('/', indexRoute);
+    app.use('/api', apiRoute);
+    app.use('/categories', categorieRoute);
+    app.use('/items', itemRoute);
+    app.use('/products', productRoute);
+    app.use('/users', userRoute);
+    app.use('/visits', visitRoute);
+    app.use('/trends', trendsRoute);
+    app.use('/highlights', highlightsRoute);
 
-module.exports = app;
+    module.exports = app;
